8392593aee52e319103d33863c13461c43590d88
DownInDaNang <ericmauricegreen2006@gmail.com>
2025-10-20 16:21:37 -0500

    Create Max All Upgrades.lua

---
+++ Broken Bones IV/Max All Upgrades.lua
@@ -0,0 +1,60 @@
+--[[
+
+	@DownInDaNang
+
+]]
+
+local functions = game.ReplicatedStorage:WaitForChild("Functions")
+
+_G.breakslevel = 500
+_G.speedlevel = 500
+_G.jumplevel = 500
+_G.sprainslevel = 500
+_G.dislocationslevel = 500
+_G.flightlevel = 500
+_G.fuellevel = 500
+
+hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
+    local method = getnamecallmethod()
+    
+    if method == "InvokeServer" and self.Parent == functions then
+        local result = {self[method](self, ...)}
+        
+        if result[1] and type(result[1]) == "table" then
+            result[1].breakslevel = _G.breakslevel
+            result[1].speedlevel = _G.speedlevel
+            result[1].jumplevel = _G.jumplevel
+            result[1].sprainslevel = _G.sprainslevel
+            result[1].dislocationslevel = _G.dislocationslevel
+            result[1].flightlevel = _G.flightlevel
+            result[1].fuellevel = _G.fuellevel
+        end
+        
+        return unpack(result)
+    end
+    
+    return self[method](self, ...)
+end))
+
+for _, connection in pairs(getconnections(functions.SendData.OnClientEvent)) do
+    if connection.Function then
+        local oldFunc = connection.Function
+        connection:Disable()
+        
+        functions.SendData.OnClientEvent:Connect(function(data, ...)
+            if data then
+                data.breakslevel = _G.breakslevel
+                data.speedlevel = _G.speedlevel
+                data.jumplevel = _G.jumplevel
+                data.sprainslevel = _G.sprainslevel
+                data.dislocationslevel = _G.dislocationslevel
+                data.flightlevel = _G.flightlevel
+                data.fuellevel = _G.fuellevel
+            end
+            return oldFunc(data, ...)
+        end)
+    end
+end
+
+
+print("LOADED")

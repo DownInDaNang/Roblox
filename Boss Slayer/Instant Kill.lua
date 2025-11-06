fe9aa5d3517098f743e10a2be28242a5c4320a20
DownInDaNang <ericmauricegreen2006@gmail.com>
2025-11-06 17:11:28 -0600

    Create Instant Kill.lua

---
+++ Boss Slayer/Instant Kill.lua
@@ -0,0 +1,30 @@
+local player = game.Players.LocalPlayer
+local range = 100
+
+game:GetService("RunService").Heartbeat:Connect(function()
+    if player.Character:FindFirstChild("Blade") then
+        local zone = player.Zone.Value
+        
+        if zone == 2 or zone == 1 then
+            for i = 1, 5 do
+                for _, mob in pairs(workspace.MobSpawns[i].Mobs:GetChildren()) do
+                    if mob:FindFirstChild("HumanoidRootPart") and mob:FindFirstChild("Humanoid") and (player.Character.HumanoidRootPart.Position - mob.HumanoidRootPart.Position).magnitude < range then
+                        mob.Humanoid.Health = 0
+                        game.ReplicatedStorage.Attack:FireServer(mob)
+                    end
+                end
+            end
+        end
+        
+        if zone == 3 then
+            for i = 1, 2 do
+                for _, mob in pairs(workspace.BossRoom1[i].Mobs:GetChildren()) do
+                    if mob:FindFirstChild("HumanoidRootPart") and mob:FindFirstChild("Humanoid") and (player.Character.HumanoidRootPart.Position - mob.HumanoidRootPart.Position).magnitude < range then
+                        mob.Humanoid.Health = 0
+                        game.ReplicatedStorage.Attack:FireServer(mob)
+                    end
+                end
+            end
+        end
+    end
+end)

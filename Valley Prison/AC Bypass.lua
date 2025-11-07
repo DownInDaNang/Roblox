c8b032ca58ca9621cd0f4bee05df891ea81640e7
DownInDaNang <ericmauricegreen2006@gmail.com>
2025-11-06 18:07:06 -0600

    Update AC Bypass.lua

--- Valley Prison/AC Bypass.lua
+++ Valley Prison/AC Bypass.lua
@@ -13,12 +13,16 @@ end
 for _, v in getgc() do
     if type(v) == "function" and islclosure(v) then
         local consts = debug.getconstants(v)
-        if table.find(consts, "PSTAND") or table.find(consts, "NOCLIP") or table.find(consts, "Luraph Script:") then
+        if table.find(consts, "PSTAND") or table.find(consts, "NOCLIP") or table.find(consts, "Luraph Script:") or table.find(consts, "BAVEL") then
             for i, c in consts do
-                if c == "PSTAND" or c == "BGYR" or c == "BVEL" or c == "NOCLIP" or c == "HBIN" or c == "ANIM" or c == "Luraph Script:" then
+                if c == "PSTAND" or c == "BGYR" or c == "BVEL" or c == "NOCLIP" or c == "HBIN" or c == "ANIM" or c == "BAVEL" or c == "Luraph Script:" then
                     debug.setconstant(v, i, "")
                 end
             end
+        end
+        
+        if table.find(consts, "goodbye cringelord") or table.find(consts, "got you!!! teehee!!") then
+            debug.setconstant(v, table.find(consts, true), false)
         end
     end
 end

225dabd5dbb4a0e9a36abcb6f850dceb9c16d32d
DownInDaNang <ericmauricegreen2006@gmail.com>
2025-12-05 17:17:35 -0600

    Implement anticheat bypass for TPS Street Soccer

---
+++ TPS Street Soccer/Anticheat Bypass.lua
@@ -0,0 +1,27 @@
+-- @DownInDaNang
+
+for i, v in getgc() do
+    if typeof(v) == "function" and islclosure(v) then
+        local c = debug.getconstants(v)
+        if table.find(c, "Asset Editing") or table.find(c, 23) and table.find(c, 50) and table.find(c, 65) then
+            for x = 1, #c do
+                if c[x] == 23 then
+                    debug.setconstant(v, x, 999)
+                elseif c[x] == 50 then
+                    debug.setconstant(v, x, 999)
+                elseif c[x] == 65 then
+                    debug.setconstant(v, x, 999)
+                end
+            end
+        end
+    end
+end
+
+local r = game:GetService("Workspace").FE.Actions:WaitForChild("KeepYourHeadUp")
+local o
+o = hookmetamethod(game, "__namecall", function(s, ...)
+    if s == r and getnamecallmethod() == "FireServer" and not checkcaller() then
+        return
+    end
+    return o(s, ...)
+end)

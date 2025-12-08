45b3b96cebbace8b82bc4c47a1bdb468c5402956
DownInDaNang <ericmauricegreen2006@gmail.com>
2025-12-08 03:59:08 -0600

    Create 1 Hit.lua

---
+++ untitled robot boxing/1 Hit.lua
@@ -0,0 +1,32 @@
+-- @DownInDaNang
+
+local rs = game:GetService("ReplicatedStorage")
+local input = require(rs:WaitForChild("RobotInput"))
+
+local function skibidi(cfg)
+    local dmg = 999
+    
+    for key in pairs(cfg.attackDMG) do
+        cfg.attackDMG[key] = dmg
+    end
+    
+    for key in pairs(cfg.attackForwardDMG) do
+        cfg.attackForwardDMG[key] = dmg
+    end
+    
+    for key in pairs(cfg.attackRECDMG) do
+        cfg.attackRECDMG[key] = dmg
+    end
+    
+    for key in pairs(cfg.attackForwardRECDMG) do
+        cfg.attackForwardRECDMG[key] = dmg
+    end
+end
+
+local orig = input.GetConfigForBot
+
+input.GetConfigForBot = function(bot)
+    local cfg = orig(bot)
+    skibidi(cfg)
+    return cfg
+end

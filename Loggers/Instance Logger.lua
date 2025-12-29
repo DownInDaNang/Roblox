f7fa10861ff83e909f9a6dabd4232b618441204f
DownInDaNang <ericmauricegreen2006@gmail.com>
2025-12-29 02:24:33 -0600

    Update Instance Logger.lua

--- Loggers/Instance Logger.lua
+++ Loggers/Instance Logger.lua
@@ -1,23 +1,32 @@
 local oldnew = Instance.new
-Instance.new = function(class, parent)
-    local instance = oldnew(class, parent)
-    
-    if checkcaller() then
-        local output = "Instance created\n"
-        output = output .. "Class: " .. tostring(class) .. "\n"
-        output = output .. "Name: " .. instance.Name .. "\n"
-        output = output .. "Parent: " .. (parent and parent:GetFullName() or "nil") .. "\n"
-        output = output .. "Call Stack:\n" .. debug.traceback() .. "\n"
-        
-        print("Instance created")
-        print("Class:", tostring(class))
-        print("Name:", instance.Name)
-        print("Parent:", parent and parent:GetFullName() or "nil")
-        print("Call Stack:")
-        print(debug.traceback())
-        
-        setclipboard(output)
+getgenv().Instance = setmetatable({}, {
+    __index = function(t, k)
+        if k == "new" then
+            return function(class, parent)
+                local instance = oldnew(class, parent)
+                
+                if checkcaller() then
+                    task.defer(function()
+                        local output = "Instance created\n"
+                        output = output .. "Class: " .. tostring(class) .. "\n"
+                        output = output .. "Name: " .. instance.Name .. "\n"
+                        output = output .. "Parent: " .. (instance.Parent and instance.Parent:GetFullName() or "nil") .. "\n"
+                        output = output .. "Call Stack:\n" .. debug.traceback() .. "\n"
+                        
+                        print("Instance created")
+                        print("Class:", tostring(class))
+                        print("Name:", instance.Name)
+                        print("Parent:", instance.Parent and instance.Parent:GetFullName() or "nil")
+                        print("Call Stack:")
+                        print(debug.traceback())
+                        
+                        setclipboard(output)
+                    end)
+                end
+                
+                return instance
+            end
+        end
+        return Instance[k]
     end
-    
-    return instance
-end
+})

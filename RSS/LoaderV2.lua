8cf2b4e2a67c2ad49c524426fc7f63913a3e32c8
DownInDaNang <ericmauricegreen2006@gmail.com>
2026-01-31 00:23:30 -0600

    Update kick message for access purchase details

--- RSS/LoaderV2.lua
+++ RSS/LoaderV2.lua
@@ -1,3 +1,3 @@
 game:GetService("Players").LocalPlayer:Kick(
-    "This script is now private. To request access, contact Nate in the Discord: https://discord.gg/Wd78JMWb or purchase access for $3."
+    "This script is now private. To request access, contact Nate in the Discord: https://discord.gg/Wd78JMWb or purchase permanent access for $3."
 )

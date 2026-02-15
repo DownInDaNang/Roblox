dfe2cf89777c78241c2365eeca2bb78e3cb778b6
LOL5678906 <ericmauricegreen2006@gmail.com>
2026-02-15 11:00:08 -0600

    Delete Field Goal Simulator directory

--- Field Goal Simulator/Always Perfect.lua
+++
@@ -1,9 +0,0 @@
-local o; o = hookmetamethod(game, "__namecall", function(s, ...)
-    if not checkcaller() and getnamecallmethod() == "FireServer" and s.Name == "BallEvent" then
-        local a = {...}
-        if a[1] == "getAccuracy" then
-            return o(s, a[1], 1)
-        end
-    end
-    return o(s, ...)
-end)
